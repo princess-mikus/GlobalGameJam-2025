2239a0ce4abdf2574224d06fd184a2acb42b9169
Xabier Benavides <xab.ben8@gmail.com>
2025-01-24 19:20:51 +0100

    Game and player scenes added

--- scripts/character_body_3d.gd
+++ scripts/character_body_3d.gd
@@ -1,18 +1,14 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+const SPEED = 1.0
+const JUMP_VELOCITY = 0.0
 
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
