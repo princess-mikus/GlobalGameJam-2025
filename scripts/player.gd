bcaaeccf9db2107f1268098eb1d085efb3b55112
princess-mikus <kiankocasaubon01@gmail.com>
2025-01-25 00:48:19 +0100

    Rotation almost done

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,7 +4,8 @@ const SPEED = 1.0
 const BULLET_SPEED = 5.0
 
 func _ready() -> void:
-	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	#Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	pass
 
 @onready var bullet = preload("res://scenes/bullet.tscn")
 
@@ -29,15 +30,13 @@ func _physics_process(delta: float) -> void:
 	var camera = get_node("../Camera3D")
 	var viewport := get_viewport()
 	var mousePos = viewport.get_mouse_position()
-	#$Crosshair.global_position = get_node("../Ground").to_local(camera.project_position(mousePos, 1))
-	$Crosshair.global_position = camera.project_position(mousePos, mousePos.y / 1000)
-	var newVector = Vector3($Crosshair.global_position.x, 0, $Crosshair.global_position.z)
-	$Pivot.look_at(newVector)
-	print(newVector)
+	var spaceState = get_world_3d().direct_space_state
+	var origin = camera.project_ray_origin(mousePos)
+	var end = origin + camera.project_ray_normal(mousePos) * 1000
+	var query = PhysicsRayQueryParameters3D.create(origin, end)
+	var result = spaceState.intersect_ray(query)
+	if result:
+		$Pivot.look_at(Vector3(result.position.x, 0, result.position.z))
 
 func _input(event):
-	if event is InputEventMouseButton and event.pressed and event.button_index == 1:
-		var camera = get_node("../Camera3D")
-		var from = camera.project_ray_origin(event.position)
-		var to = from + camera.project_ray_normal(event.position) * 1000
-		$Pivot.look_at(Vector3(to.x - from.x, 0, to.z - from.x))
+	pass
