e53ea83bbf0b5311be3db201532ad5b5fef35cbe
Xabier Benavides <xab.ben8@gmail.com>
2025-01-26 04:07:11 +0100

    Explosion

--- scripts/explosion.gd
+++ scripts/explosion.gd
@@ -1,15 +1,24 @@
 extends Area3D
 
+const maxSize = 1
+const explosionTime = 0.2
+
+@onready var sprite = $Sprite3D
+@onready var originalScale = sprite.transform.basis.get_scale()
+
+var size = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	#scale = Vector3(1, 1, 1)
+	sprite.scale = Vector3(0,0,0)
 	body_entered.connect(_on_body_in_area)
-	print("I'm exploding!")
-	pass
-
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _physics_process(delta: float) -> void:
+	if size < maxSize:
+		sprite.scale = size*originalScale
+		size += 1/(explosionTime*60.0)
 
 func _on_duration_timeout() -> void:
 	queue_free()
